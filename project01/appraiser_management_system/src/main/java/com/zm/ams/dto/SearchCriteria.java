0e2c25ca2b86758b8d9c32ee92a058ca0d257abd
SJagadesh123 <153438506+SJagadesh123@users.noreply.github.com>
2024-01-02 14:26:07 +0530

    Delete project01 directory

--- project01/appraiser_management_system/src/main/java/com/zm/ams/dto/SearchCriteria.java
+++
@@ -1,6 +0,0 @@
-package com.zm.ams.dto;
-
-public interface SearchCriteria {
-
-	// this is marker inteface which is used to tell which object can be used for search operations
-}
--- project01/appraiser_management_system/src/main/java/com/zm/ams/dto/ValidDto.java
+++
@@ -1,5 +0,0 @@
-package com.zm.ams.dto;
-
-public interface ValidDto {
-
-}
